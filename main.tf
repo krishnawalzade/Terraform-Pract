33ef6e369dab32978a201f159c01fbd4450aee16
krishna <krishnawalzade403@gmail.com>
2025-10-01 20:32:17 +0530

    private subnet added

--- main.tf
+++ main.tf
@@ -13,3 +13,9 @@ resource "aws_subnet" "public_subnet" {
 
   tags = {Name = "pubs"}
 }
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id     = aws_vpc.myvpc.id
+  cidr_block = "10.0.2.0/24"
+  tags = { Name = "private_sub"}
+}
